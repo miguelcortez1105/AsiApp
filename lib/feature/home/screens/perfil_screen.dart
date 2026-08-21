85bcd4d9eae8b86871ac6213cb4d5e5e3bcb65c6
Ana Luísa Silva Alves <d2026002228@unifei.edu.br>
2026-08-21 00:51:24 -0300

    ícone avatar no início

---
+++ lib/feature/home/screens/perfil_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class PerfilScreen extends StatefulWidget {
+  const PerfilScreen({super.key});
+
+  @override
+  State<PerfilScreen> createState() {
+    // TODO: implement createState
+    throw _PerfilScreenState();
+  }
+}
+
+class _PerfilScreenState extends State<PerfilScreen>{
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _senhaController = TextEditingController();
+
+  String cargo = 'Membro';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meu Perfil'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment:CrossAxisAlignment.start,
+          children: [
+            const Center(
+              child: CircleAvatar(
+                radius: 50,
+                child: Icon(Icons.person, size: 50),
+              )
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
